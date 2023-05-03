c12e7db9da1efbb9784bdcae6817854e9865be3b
eunanMur <50799208+eunanMur@users.noreply.github.com>
2023-05-03 18:03:43 +0100

    Updated the config

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCdkK5HgBLjr9XVcAbNF32AgxIIkCaeB74",
-  authDomain: "website-d9701.firebaseapp.com",
-  databaseURL: "https://website-d9701-default-rtdb.firebaseio.com",
-  projectId: "website-d9701",
-  storageBucket: "website-d9701.appspot.com",
-  messagingSenderId: "405871353274",
-  appId: "1:405871353274:web:c7cca232d296e6fe349183",
-  measurementId: "G-3VTKXJVGVT"
+  apiKey: "AIzaSyAQ7bxMK7_ETzf3BG3u2iSHUTRxjdRrr_c",
+  authDomain: "tempem-4c764.firebaseapp.com",
+  databaseURL: "https://tempem-4c764-default-rtdb.firebaseio.com",
+  projectId: "tempem-4c764",
+  storageBucket: "tempem-4c764.appspot.com",
+  messagingSenderId: "201376466206",
+  appId: "1:201376466206:web:0baa6a74e0561bfd85750f",
+  measurementId: "G-Q4BS2EHRPC"
 };
 firebase.initializeApp(firebaseConfig);
 
