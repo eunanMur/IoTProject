d5e4313aa9dd9d6bd772022a3bb41fa5bdd478e3
eunanMur <50799208+eunanMur@users.noreply.github.com>
2023-05-03 18:04:21 +0100

    Updated Firebase config

--- form.js
+++ form.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyCH14gTyKTXsvRI7VWfb54wM2tl0goBLug",
-  	authDomain: "website-ab1d4.firebaseapp.com",
-  	databaseURL: "https://website-ab1d4-default-rtdb.firebaseio.com",
-  	projectId: "website-ab1d4",
-  	storageBucket: "website-ab1d4.appspot.com",
-  	messagingSenderId: "249497437283",
-  	appId: "1:249497437283:web:54b1aed70d8d73f6e4a456",
-  	measurementId: "G-GR8NW4NFCW"
+    apiKey: "AIzaSyAQ7bxMK7_ETzf3BG3u2iSHUTRxjdRrr_c",
+    authDomain: "tempem-4c764.firebaseapp.com",
+    databaseURL: "https://tempem-4c764-default-rtdb.firebaseio.com",
+    projectId: "tempem-4c764",
+    storageBucket: "tempem-4c764.appspot.com",
+    messagingSenderId: "201376466206",
+    appId: "1:201376466206:web:0baa6a74e0561bfd85750f",
+    measurementId: "G-Q4BS2EHRPC"
 //firebase config stuff
   };
   // Initialize Firebase
